0418d1f618ac48b8d3d9c9f210f5e1e2ad1605ee
YuZhong-Chen <yuzhong1214@gmail.com>
2024-04-18 22:54:09 +0800

    Publish the reward with timestamp when step the Env.

--- kobuki_zed_vlp16_ws/workspaces/kobuki_ws/src/gazebo_rl_env/gazebo_rl_env/gazebo_rl_env.py
+++ kobuki_zed_vlp16_ws/workspaces/kobuki_ws/src/gazebo_rl_env/gazebo_rl_env/gazebo_rl_env.py
@@ -5,6 +5,8 @@ from gazebo_msgs.srv import GetEntityState
 from gazebo_msgs.srv import SpawnEntity, DeleteEntity
 from std_srvs.srv import Empty
 
+from std_msgs.msg import Float32MultiArray, MultiArrayDimension
+
 from ament_index_python.packages import get_package_share_directory
 
 import os
@@ -21,9 +23,12 @@ class GAZEBO_RL_ENV_NODE(Node):
             "step_time_delta": 0.05,  # seconds
             "gazebo_service_timeout": 1.0,  # seconds
             "reach_target_distance": 0.2,  # meters
+            "target_reward": 100,
+            "penalty_per_step": -0.01,
         }
 
         self.current_timestamp = 0
+        self.current_reward = 0.0
 
         # Create the clients for the Gazebo services
         self.pause_client = self.create_client(Empty, "/pause_physics")
@@ -33,14 +38,18 @@ class GAZEBO_RL_ENV_NODE(Node):
         self.delete_entity_client = self.create_client(DeleteEntity, "/delete_entity")
         self.get_entity_state_client = self.create_client(GetEntityState, "/get_entity_state")
 
+        # Create the publisher for the environment
+        self.info_publisher = self.create_publisher(Float32MultiArray, "/rl_env/info", 10)
+
         # Load the ball URDF
         ball_urdf_path = os.path.join(get_package_share_directory("gazebo_rl_env"), "urdf", "ball.urdf")
         self.ball_urdf = open(ball_urdf_path, "r").read()
-        
+
         self.target_list = self.generate_target()
 
     def reset(self):
         self.current_timestamp = 0
+        self.current_reward = 0.0
 
         while not self.reset_world_client.wait_for_service(timeout_sec=self.config["gazebo_service_timeout"]):
             self.get_logger().info('Gazebo service "reset_world" not available, waiting again...')
@@ -64,7 +73,10 @@ class GAZEBO_RL_ENV_NODE(Node):
         while not self.pause_client.wait_for_service(timeout_sec=self.config["gazebo_service_timeout"]):
             self.get_logger().info('Gazebo service "pause" not available, waiting again...')
         self.pause_client.call_async(Empty.Request())
-        
+
+        # Reset the reward
+        self.current_reward = self.config["penalty_per_step"]
+
         # Check whether the Kobuki reaches the target
         state = self.get_kobuki_state()
         for target in self.target_list:
@@ -72,7 +84,11 @@ class GAZEBO_RL_ENV_NODE(Node):
             if distance < self.config["reach_target_distance"]:
                 self.delete_ball(target.name)
                 self.target_list.remove(target)
+                self.current_reward = self.config["target_reward"]
                 self.get_logger().info(f"Kobuki reaches target {target.name} at timestamp {self.current_timestamp}")
+        
+        # Publish the information
+        self.publish_info()
 
     def spawn_ball(self, x: float, y: float, z: float, name: str):
         self.spawn_ball_request = SpawnEntity.Request()
@@ -129,10 +145,10 @@ class GAZEBO_RL_ENV_NODE(Node):
 
     def generate_target(self):
         target_list = []
-        
+
         # Generate 10 targets with random y coordinates, and fixed x and z coordinates
         for i in range(10):
-            x = 2.0 * (i + 1)
+            x = 1.0 * (i + 1)
             y = np.random.uniform(-1.0, 1.0)
             z = 0.2
             name = "target_" + str(i)
@@ -141,13 +157,28 @@ class GAZEBO_RL_ENV_NODE(Node):
 
         return target_list
 
+    def publish_info(self):
+        msg = Float32MultiArray()
+
+        # Add the timestamp
+        msg.layout.dim.append(MultiArrayDimension(label="timestamp", size=1, stride=1))
+        msg.data.append(self.current_timestamp)
+
+        # Add the reward
+        msg.layout.dim.append(MultiArrayDimension(label="reward", size=1, stride=1))
+        msg.data.append(self.current_reward)
+
+        # Publish the message
+        self.info_publisher.publish(msg)
+
+
 class TARGET:
     def __init__(self, x: float, y: float, z: float, name: str):
         self.x = x
         self.y = y
         self.z = z
         self.name = name
-    
+
     def get_distance(self, state):
         # Calculate the Euclidean distance between the target and the state,
         # note that we only consider the x and y coordinates.
